b1005f8ffe026acba93add4c8e9f96ced8cb2c33
almerindo <almerindo.rehem@gmail.com>
2021-06-21 19:38:36 -0300

    feat: add connsole.log

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import packageJson from '../package.json'
 const app = express();
 
 app.get('/', (request, response) => {
+  console.info('Teste');
   return response.json({ message: 'Hello Quero', version: packageJson.version })
 })
 
