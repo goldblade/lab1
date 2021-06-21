8a997f1cc59554b4ee22a702ecad983994056de0
almerindo <almerindo.rehem@gmail.com>
2021-06-21 19:33:15 -0300

    feat: add initial structure

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import packageJson from '../package.json'
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello Quero', version: packageJson.version })
+})
+
+app.listen(3333);
